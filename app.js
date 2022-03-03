f82f1215ebce3396229ab2049d1f7c91e01b6ba1
Frank Udoags <frankudoags@gmail.com>
2022-03-03 22:55:19 +0100

    JS update

--- app.js
+++ app.js
@@ -61,7 +61,13 @@ guessBtn.addEventListener('click', function(){
                 //game over --lost 
                 gameOver(false,`Game over, you lost.The correct number was ${winningNum}`);
             }
-             
+             if(guess + 5 > winningNum || guess - 5 < winningNum) {
+                        //change border color 
+                    guessInput.style.border = '1px solid red';
+                    //clear the input
+                    guessInput.value = '';
+                    setMessage(`Lmao, ${guessesLeft} guesses left`, 'red');
+            }
                 
                else if(guess > winningNum) {
                             //change border color 
